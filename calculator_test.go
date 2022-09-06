397e31f642a5b5e6826af6b1bb771c6e98c3fc40
onejulian2014 <onejulian2014@gmail.com>
2022-09-05 21:59:24 -0500

    :sparkles: a simple calculator test

---
+++ calculator_test.go
@@ -0,0 +1,16 @@
+package calculator_test
+
+import (
+	"testing"
+	"testModule/calculator"
+)
+
+func TestAdd(t *testing.T) {
+	sum := calculator.Add(1,2)
+	if sum != 3 {
+		t.Error("La suma no es correcta")
+		t.Fail()
+	} else {
+		t.Log("Test finalizado correctamente")
+	}
+}	
---
+++ go.mod
@@ -0,0 +1,3 @@
+module testModule
+
+go 1.19
